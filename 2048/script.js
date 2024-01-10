299c72c95325279ab121abb6ef6d202ef4fb91b9
Daniel Jung <rain4daniel@gmail.com>
2024-01-10 11:16:24 -0500

    Mobile friendly navbar

--- 2048/script.js
+++ 2048/script.js
@@ -259,3 +259,37 @@ document.addEventListener("DOMContentLoaded", function () {
     initializeGame();
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var header = document.querySelector(".header");
+  var didScroll = false;
+
+  window.addEventListener("scroll", function () {
+    didScroll = true;
+  });
+
+  setInterval(function () {
+    if (didScroll) {
+      handleScroll();
+      didScroll = false;
+    }
+  }, 250);
+
+  function handleScroll() {
+    var scrollTop = window.scrollY;
+
+    if (scrollTop > 50) {
+      header.classList.add("hide-header");
+    } else {
+      header.classList.remove("hide-header");
+    }
+  }
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
